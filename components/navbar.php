e9febe7678b361f13763fb5fd886c2488bb4c89f
Tapedeuxfoistoctoc <piikme7@gmail.com>
2023-07-20 09:06:40 +0400

    up

--- components/navbar.php
+++ components/navbar.php
@@ -1,6 +1,77 @@
-<nav>
-    <a href="">blog</a>
-    <a href="">amerique</a>
-    <a href="">asie</a>
-    <a href="">contact</a>
-</nav>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>Viaje</title>
+    <link rel="icon" href="./cache/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <style>
+        /* Styles spécifiques pour la navbar */
+        .navbar {
+            background-color: #333;
+            padding: 10px;
+            margin-top: 20px;
+        }
+
+        .navbar a {
+            color: white;
+            font-size: 18px;
+            margin-right: 10px;
+            text-decoration: none;
+        }
+
+        .navbar a:hover {
+            color: #4CAF50;
+        }
+
+        .search-container {
+            display: inline-block;
+            margin-left: auto;
+        }
+
+        .search-container input[type="text"] {
+            padding: 5px;
+            border-radius: 3px;
+            border: none;
+        }
+
+        .search-container button {
+            padding: 5px 10px;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            cursor: pointer;
+        }
+    </style>
+</head>
+
+<body>
+    <div>
+        <!-- Blason ici -->
+    </div>
+    <nav class="navbar">
+        <a href="">BLOG VOYAGE</a>
+        <a href="">PRATIQUE</a>
+        <a href="">AMÉRIQUE</a>
+        <a href="">ASIE</a>
+        <a href="">EUROPE</a>
+        <a href="">AFRIQUE & MOYEN-ORIENT</a>
+        <a href="">OCÉANIE</a>
+        <a href="">CONTACT</a>
+        <div class="search-container">
+            <input type="text" placeholder="Rechercher...">
+            <button type="submit">Rechercher</button>
+        </div>
+    </nav>
+    <section>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quas totam autem earum cupiditate, eum repellat dolorum veritatis mollitia, natus aperiam debitis provident aliquam magnam optio eius architecto doloribus ipsam?</p>
+    </section>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+</html>
+
+
